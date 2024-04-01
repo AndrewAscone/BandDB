8debc60461180ce24046b786c54bcc999588a9ab
AndrewAscone <andrew.ascone@gmail.com>
2024-04-01 18:21:37 -0400

    First commit

---
+++ src/test/java/com/Scone/BandDB/BandDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scone.BandDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
