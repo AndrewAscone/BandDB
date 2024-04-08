551cfcdf7808e3589c80fce2935bbb4a9a563d3b
AndrewAscone <andrew.ascone@gmail.com>
2024-04-08 17:43:37 -0400

    Working on Musician class

--- src/main/java/com/Scone/BandDB/Musician.java
+++ src/main/java/com/Scone/BandDB/Musician.java
@@ -1,4 +1,16 @@
 package com.Scone.BandDB;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Musician {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String instrument;
 }
