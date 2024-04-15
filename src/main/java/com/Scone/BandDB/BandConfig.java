1a5eed252da011df6b694a858571e1123c5f06fb
AndrewAscone <andrew.ascone@gmail.com>
2024-04-15 18:49:02 -0400

    Beginning to work on BandConfig setup

--- src/main/java/com/Scone/BandDB/BandConfig.java
+++ src/main/java/com/Scone/BandDB/BandConfig.java
@@ -1,4 +1,18 @@
 package com.Scone.BandDB;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BandConfig {
+    @Autowired
+    private BandRepository bandRepository;
+    @Autowired
+    private MusicianRepository musicianRepository;
+
+    @PostConstruct
+    public void setup(){
+
+    }
 }
