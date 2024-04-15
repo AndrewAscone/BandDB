747222a8504f7f6abe041dc938e0cb20dddac251
AndrewAscone <andrew.ascone@gmail.com>
2024-04-15 19:09:32 -0400

    Setting up first band for initial config

--- src/main/java/com/Scone/BandDB/BandConfig.java
+++ src/main/java/com/Scone/BandDB/BandConfig.java
@@ -4,6 +4,9 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class BandConfig {
     @Autowired
@@ -14,5 +17,35 @@ public class BandConfig {
     @PostConstruct
     public void setup(){
         Musician musician1 = new Musician();
+        musician1.setFirstName("Trey");
+        musician1.setLastName("Anastasio");
+        musician1.setInstrument("Guitar");
+
+        Musician musician2 = new Musician();
+        musician2.setFirstName("Mike");
+        musician2.setLastName("Gordon");
+        musician2.setInstrument("Bass");
+
+        Musician musician3 = new Musician();
+        musician3.setFirstName("Page");
+        musician3.setLastName("McConnell");
+        musician3.setInstrument("Keys");
+
+        Musician musician4 = new Musician();
+        musician4.setFirstName("Jon");
+        musician4.setLastName("Fishman");
+        musician4.setInstrument("Drums");
+
+        List<Musician> firstBandMembers = new ArrayList<>();
+        firstBandMembers.add(musician1);
+        firstBandMembers.add(musician2);
+        firstBandMembers.add(musician3);
+        firstBandMembers.add(musician4);
+
+        Band band1 = new Band();
+        band1.setBandName("Phish");
+        band1.setYearFormed(1983);
+        band1.setMembers(firstBandMembers);
+
     }
 }
