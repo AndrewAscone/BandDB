5637bd6266fcc01d16f27c062a7d6dffbf4e2811
AndrewAscone <andrew.ascone@gmail.com>
2024-04-23 17:27:07 -0400

    Beginning work on musician service

--- src/main/java/com/Scone/BandDB/MusicianService.java
+++ src/main/java/com/Scone/BandDB/MusicianService.java
@@ -1,4 +1,14 @@
 package com.Scone.BandDB;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MusicianService {
+    private MusicianRepository musicianRepository;
+
+    @Autowired
+    public MusicianService(MusicianRepository musicianRepository){
+        this.musicianRepository = musicianRepository;
+    }
 }
