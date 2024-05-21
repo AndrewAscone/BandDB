2891d82153c88a56d26057f2196cb63f70cc7c4f
AndrewAscone <andrew.ascone@gmail.com>
2024-05-21 17:58:48 -0400

    Added create function to musician controller

--- src/main/java/com/Scone/BandDB/MusicianController.java
+++ src/main/java/com/Scone/BandDB/MusicianController.java
@@ -1,4 +1,24 @@
 package com.Scone.BandDB;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class MusicianController {
+    private final MusicianService musicianService;
+
+    @Autowired
+    public MusicianController(MusicianService musicianService){
+        this.musicianService = musicianService;
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<Musician> create(@RequestBody Musician musician){
+        return new ResponseEntity<>(musicianService.create(musician), HttpStatus.CREATED);
+    }
 }
