eba5b8d5624c9a34e27f509b005b19838f0df38f
AndrewAscone <andrew.ascone@gmail.com>
2024-06-15 15:57:30 -0400

    Added read by id, read all, and delete functions to BandController

--- src/main/java/com/Scone/BandDB/BandController.java
+++ src/main/java/com/Scone/BandDB/BandController.java
@@ -5,9 +5,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/band")
@@ -23,5 +23,20 @@ public class BandController {
     @PostMapping(value = "/create")
     public ResponseEntity<Band> create(@RequestBody Band band){
         return new ResponseEntity<>(bandService.create(band), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/read/{id}")
+    public ResponseEntity<Band> readById(@PathVariable Long id){
+        return new ResponseEntity<>(bandService.readById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/readAll")
+    public ResponseEntity<List<Band>> readAll(){
+        return new ResponseEntity<>(bandService.readAll(), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Band> deleteById(@PathVariable Long id){
+        return new ResponseEntity<>(bandService.deleteById(id), HttpStatus.OK);
     }
 }
